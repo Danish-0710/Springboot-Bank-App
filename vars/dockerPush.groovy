777d5c2feb7944cfa1b8486da4b089c7a4f922ef
Danish Ahmed Siddiqui <danishaisid786@gmail.com>
2024-11-20 13:28:32 +0530

    Update and rename dockerpush.groovy to dockerPush.groovy

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,6 @@
+def dockerPush(String project, String imageTag, String dockerHubUser) {
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  }
+  sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+}
